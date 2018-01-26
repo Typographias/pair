fd8e4afdee2e8b2874767aefa7ec41d819569bf3
Thiago Oliveira <thiago@signaes.com>
2018-01-26 08:50:14 -0200

    Add feature to read string from a .txt file

--- pair.go
+++ pair.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 	"strings"
 )
 
-func main() {
-	args := strings.Join(os.Args[1:], "")
-	glyphs := strings.Split(args, "")
-
+func combine(glyphs []string) {
 	for _, glyph := range glyphs {
 		fmt.Println(glyph)
 
@@ -19,5 +18,26 @@ func main() {
 		}
 
 		fmt.Print("\n")
+	}
+}
+
+func glyphs(str string) []string {
+	return strings.Split(str, "")
+}
+
+func main() {
+	args := strings.Join(os.Args[1:], "")
+
+	if strings.HasSuffix(args, ".txt") {
+		filepath := args
+		fileBytes, err := ioutil.ReadFile(filepath)
+		if err != nil {
+			log.Fatal("Failed to read file: " + filepath)
+		}
+
+		str := strings.Trim(string(fileBytes), "\n")
+		combine(glyphs(str))
+	} else {
+		combine(glyphs(args))
 	}
 }
