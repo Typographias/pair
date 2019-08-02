d37330e068b78654bc8dea1fe54b6c424f1eaed6
Thiago Oliveira <thiago@signaes.com>
2019-08-02 00:34:17 -0300

    Add split options to split results between a file for each subject glyph

--- pair.go
+++ pair.go
@@ -1,12 +1,43 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"strings"
 )
+
+var split = flag.Bool("split", false, "Split results in multiple files")
+
+func printToFiles(glyphs []string) {
+	dir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	for _, glyph := range glyphs {
+		filename := dir + glyph + "_kerning_pairs.txt"
+
+		fmt.Println("Creating ", filename)
+
+		file, err := os.Create(filename)
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+
+		file.WriteString(glyph + "\n")
+
+		for _, pair := range glyphs {
+			file.WriteString(pair + glyph + pair)
+			file.WriteString(pair + glyph + pair + " ")
+		}
+
+		file.WriteString("\n")
+	}
+}
 
 func combine(glyphs []string) {
 	for _, glyph := range glyphs {
@@ -26,7 +57,9 @@ func glyphs(str string) []string {
 }
 
 func main() {
-	args := strings.Join(os.Args[1:], "")
+	flag.Parse()
+
+	args := strings.Join(flag.Args()[0:1], "")
 
 	if strings.HasSuffix(args, ".txt") {
 		filepath := args
@@ -36,6 +69,11 @@ func main() {
 		}
 
 		str := strings.Trim(string(fileBytes), "\n")
+
+		if *split {
+			printToFiles(glyphs(str))
+		}
+
 		combine(glyphs(str))
 	} else {
 		combine(glyphs(args))
